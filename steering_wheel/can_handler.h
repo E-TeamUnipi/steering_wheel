53cdec58970a2f8367f79a3a4fa2e7c5bd56400c
Giuseppe Fabiano <gfabiano40@gmail.com>
2019-04-23 18:34:34 +0200

    [can_handler] removed ring buffer. now changes are handled by volatile compact struct, simplify also communication with the display

--- steering_wheel/can_handler.h
+++ steering_wheel/can_handler.h
@@ -2,17 +2,52 @@
 #define can_handler_h
 
 #include <due_can.h>
-#include <RingBuf.h>
+#include "byteorder.h"
+
+#define ID_GEAR 0x601
+
+namespace {
+    struct can_id_pos {
+        uint16_t id;
+        uint8_t pos;
+    };
+}
+
 
 namespace can_handler {
-    RingBuf<CAN_FRAME, 64> m_ring;
+
+    static constexpr struct {
+        const can_id_pos RPM{0x600, 1};
+        const can_id_pos GEAR{0x601, 1};
+    } ids;
+
+    struct __attribute__((packed)) {
+        uint8_t sep[4]{'A', 'C', 'K', 25};
+        uint16_t RPM{0};
+        uint16_t TPS{0};
+        uint16_t MAP{0};
+    } volatile hz25;
+
+    struct __attribute__((packed)) {
+        uint8_t sep[4]{'A', 'C', 'K', 3};
+        uint16_t EOT{20}; // engine oil temp
+        uint16_t ECT{20}; // engine coolant(water) temp
+        uint16_t EOP{20}; // engine oil pressure
+
+        uint16_t BATTERY{0};
+
+        uint8_t GEAR{1};
+
+        uint8_t LIMP;
+        uint8_t LAUNCH{0};
+    } volatile hz3;
 }
 
 // hidden
 namespace {
     CANRaw *g_can{nullptr};
     constexpr uint32_t can_lst_id{0x600};
-    constexpr uint32_t can_hst_id{0x625};
+    constexpr uint32_t can_hst_id{0x640};
 
     // Here we receive frames, we filter fast and add
     // it to the ring
@@ -21,9 +56,17 @@ namespace {
         // TODO: This is an example to remember
         //       the logic.
         switch(t_frame->id) {
-            case 0x600:
-            case 0x601: {
-                can_handler::m_ring.push(*t_frame);
+            case can_handler::ids.RPM.id: {
+                can_handler::hz25.RPM = byteorder::ctohs(t_frame->data.s0);
+                break;
+            }
+            case ID_GEAR: {
+                can_handler::hz3.GEAR = t_frame->data.bytes[0];
+                break;
+            }
+            case 0x603: {
+                can_handler::hz3.EOT = byteorder::ctohs(t_frame->data.s0);
+                break;
             }
         }
     }
@@ -33,9 +76,9 @@ namespace can_handler {
     bool init()
     {
         g_can = []() -> CANRaw * {
-            if (Can0.begin(CAN_BPS_1000K, 62)) {
+            if (Can0.begin(CAN_BPS_1000K)) {
                 return &Can0;
-            } else if (Can1.begin(CAN_BPS_1000K, 65)) {
+            } else if (Can1.begin(CAN_BPS_1000K)) {
                 return &Can1;
             }
             return nullptr;
@@ -44,6 +87,7 @@ namespace can_handler {
         if (g_can) {
             g_can->watchForRange(can_lst_id, can_hst_id);
             g_can->setGeneralCallback(routine);
+
             return true;
         }
         
