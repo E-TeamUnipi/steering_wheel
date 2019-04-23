9ccc9594595923b3d0a2640abf9e1b3338b88825
Giuseppe Fabiano <gfabiano40@gmail.com>
2019-04-23 18:38:34 +0200

    [steering_wheel] simplified main with new modules

--- steering_wheel/steering_wheel.ino
+++ steering_wheel/steering_wheel.ino
@@ -1,9 +1,9 @@
-#include <DueTimer.h>
-#include <RingBuf.h>
-
 #include "byteorder.h"
 #include "display.h"
 #include "can_handler.h"
+
+#define SOFTWARE_DEBOUNCING 1
+
 #include "click_handler.h"
 
 // Remove loop overhead, never called!
@@ -13,46 +13,9 @@ void setup()
 {
     Serial.begin(115200);
 
+    while(!can_handler::init());
     click_handler::init();
-    Serial.println("I'm ready!");
-
-    //Serial.println(htonl(0xff00ff00), HEX);
-
-    // test canbus byteorder
-    Serial.print("long \t0x");
-    Serial.print(byteorder::ctohl(0xff00ff00), HEX);
-    Serial.print(" \t0x");
-    Serial.print(0x00ff00ff, HEX);
-    if (byteorder::ctohl(0xff00ff00) == 0x00ff00ff) {
-        Serial.println(" \tPASSED!");
-    } else {
-        Serial.println(" \tFAILED!");
-    }
-
-    Serial.print("short \t0x");
-    Serial.print(byteorder::ctohs(0xff00), HEX);
-    Serial.print(" \t\t0x");
-    Serial.print(0x00ff, HEX);
-    if (byteorder::ctohs(0xff00) == 0x00ff) {
-        Serial.println(" \t\tPASSED!");
-    } else {
-        Serial.println(" \t\tFAILED!");
-    }
-
-    // Test to send data with interrupts
-    Timer0.attachInterrupt([](){
-        static uint8_t msg_id{0xf0};
-        static uint16_t msg{0};
-
-        CAN_FRAME outgoing;
-        outgoing.id = msg_id;
-        outgoing.data.s0 = msg;
-
-        ++msg;
-        msg_id = (msg_id ==0x40)? 0xf0:0x40;
-        
-        can_handler::m_ring.push(outgoing);
-    }).setFrequency(2000).start();
+    display::init();
 
     _main();
 }
@@ -60,43 +23,20 @@ void setup()
 void _main()
 {
     uint8_t count{0};
+
     for(;;) {
-        uint8_t data;
+        uint8_t pin;
         CAN_FRAME frame;
-        if (click_handler::m_ring.lockedPop(data)) {
-            Serial.print(data);
-            Serial.print(" clicks " );
-            Serial.println(++count);
-
+        if (click_handler::m_ring.lockedPop(pin)) {
             CAN_FRAME outgoing;
-            outgoing.id = 0x400;
+            outgoing.id = 0x001;
             outgoing.extended = false;
-            outgoing.priority = 4; //0-15 lower is higher priority
-            outgoing.data.byte[2] = 0xDD;
-
-            outgoing.data.s0 = 0xFEED;
-            outgoing.data.byte[3] = 0x55;
-            outgoing.data.high = 0xDEADBEEF;
-            can_handler::send(outgoing);
+            outgoing.priority = 4;
+            outgoing.length = 1;
+            outgoing.data.s0 = count;
         }
 
-        if (can_handler::m_ring.lockedPop(frame)) {
-//            Serial.print(" ID: 0x");
-//            Serial.print(frame.id, HEX);
-//            Serial.print(" Len: ");
-//            Serial.print(frame.length);
-//            Serial.print(" Data: 0x");
-//            for (int count = 0; count < frame.length; count++) {
-//                Serial.print(frame.data.bytes[count], HEX);
-//                Serial.print(" ");
-//            }
-//            Serial.print("\r\n");
-//              display::send(frame.id, frame.data.s0);
-    
-        }
-
-
-
+        display::update();
     }
 }
 
