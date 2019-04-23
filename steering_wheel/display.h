ffa8347b0f660002f6ada35446291bcf1f3011a7
Giuseppe Fabiano <gfabiano40@gmail.com>
2019-04-23 18:37:32 +0200

    [display] New display protocol that handle better errors, is faster and does not fill the serial bus

--- steering_wheel/display.h
+++ steering_wheel/display.h
@@ -1,35 +1,49 @@
 #ifndef display_h
 #define display_h
 
+#include <DueTimer.h>
+
+// to get directly access to can structures
+#include "can_handler.h"
+
 // hidden
 namespace {
-
-    // send to uart serial port data in this format:
-    // 
-    // |        delimiter       |   id   |     data       |   crc == id    |
-    // |         3 bytes        | 1 byte |    2 bytes     |    2 bytes     |
-    //
-    // crc for now not used, we need it fastest as possible,
-    // if there are errors it isn't a problem because error converge
-    // fastly to the right value. 
-    struct __attribute__((packed)) display_msg {
-        uint8_t m_delimiter[3];
-        uint8_t m_id;
-        uint16_t m_msg;
-        uint16_t crc;
-
-        display_msg(uint8_t t_id, uint16_t t_msg) :
-            m_delimiter{0xff, 0xff, 0xff},
-            m_id(t_id), m_msg(t_msg), crc{t_id} {}
-    };
+    struct {
+        bool hertz_25{false};
+        bool hertz_3{false};  
+    } volatile clocks;
 }
 
 namespace display {
     inline
-    void send(uint8_t msg_id, uint16_t msg)
+    void update()
     {
-        display_msg buf(msg_id, msg);
-        Serial.write(reinterpret_cast<uint8_t *>(&buf), 6);
+        if (clocks.hertz_25) {
+            clocks.hertz_25 = false;
+            Serial.write(
+                (uint8_t *) &can_handler::hz25,
+                sizeof(can_handler::hz25)
+            );
+        }
+
+        if (clocks.hertz_3) {
+            clocks.hertz_3 = false;
+            Serial.write(
+                (uint8_t *) &can_handler::hz3,
+                sizeof(can_handler::hz3)
+            );
+        }
+    }
+
+    void init()
+    {
+        Timer0.attachInterrupt([]() {
+            clocks.hertz_25 = true;
+        }).setFrequency(25).start();
+
+        Timer1.attachInterrupt([]() {
+            clocks.hertz_3 = true;
+        }).setFrequency(3).start();
     }
 }
 
